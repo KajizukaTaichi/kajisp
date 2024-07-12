590006a289528bfb45c1a39b05dd291fd930a055
梶塚太智 <kajizukataichi@outlook.jp>
2024-07-12 20:53:39 +0900

    Implement string type

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,7 @@ enum SExpression {
 enum Type {
     Number(f64),
     Symbol(String),
+    String(String),
     Nil,
 }
 
@@ -47,6 +48,7 @@ impl Type {
         match self {
             Type::Number(i) => *i,
             Type::Symbol(s) => s.parse().unwrap_or(0f64),
+            Type::String(s) => s.parse().unwrap_or(0f64),
             Type::Nil => 0f64,
         }
     }
@@ -55,7 +57,17 @@ impl Type {
         match self {
             Type::Number(i) => i.to_string(),
             Type::Symbol(s) => s.to_string(),
+            Type::String(s) => format!("\"{s}\""),
             Type::Nil => "nil".to_string(),
+        }
+    }
+
+    fn get_string(&self) -> String {
+        match self {
+            Type::Number(i) => i.to_string(),
+            Type::Symbol(s) => s.to_string(),
+            Type::String(s) => s.to_string(),
+            Type::Nil => String::new(),
         }
     }
 }
@@ -65,49 +77,86 @@ fn execute(program: SExpression) -> Type {
         return value;
     } else if let SExpression::List(list) = program {
         let list: Vec<Type> = list.iter().map(|i| execute(i.clone())).collect();
-        let command: Type = list
-            .get(0)
-            .expect("チノちゃん「うるさいですね...」")
-            .clone();
+        let command: Type = if let Some(i) = list.get(0) {
+            i.clone()
+        } else {
+            return Type::Nil;
+        };
         let params: Vec<Type> = list[1..list.len()].to_vec();
 
         match command.get_symbol().as_str() {
             "+" => Type::Number({
                 let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
 
-                let mut sum: f64 = params[0];
+                let mut result: f64 = *params.get(0).expect("The paramater is deficiency");
                 for i in params[1..params.len()].to_vec().iter() {
-                    sum += i;
+                    result += i;
                 }
-                sum
+                result
             }),
             "-" => Type::Number({
                 let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
 
-                let mut sum: f64 = params[0];
+                let mut result: f64 = *params.get(1).expect("The paramater is deficiency");
                 for i in params[1..params.len()].to_vec().iter() {
-                    sum -= i;
+                    result -= i;
                 }
-                sum
+                result
             }),
             "*" => Type::Number({
                 let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
 
-                let mut sum: f64 = params[0];
+                let mut result: f64 = *params.get(1).expect("The paramater is deficiency");
                 for i in params[1..params.len()].to_vec().iter() {
-                    sum *= i;
+                    result *= i;
                 }
-                sum
+                result
             }),
             "/" => Type::Number({
                 let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
 
-                let mut sum: f64 = params[0];
+                let mut result: f64 = *params.get(1).expect("The paramater is deficiency");
                 for i in params[1..params.len()].to_vec().iter() {
-                    sum /= i;
+                    result /= i;
                 }
-                sum
+                result
             }),
+            "%" => Type::Number({
+                let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
+
+                let mut result: f64 = *params.get(1).expect("The paramater is deficiency");
+                for i in params[1..params.len()].to_vec().iter() {
+                    result %= i;
+                }
+                result
+            }),
+            "concat" => Type::String(params.iter().map(|i| i.get_string()).collect()),
+            "println" => {
+                println!(
+                    "{}",
+                    params
+                        .get(1)
+                        .expect("The paramater is deficiency")
+                        .get_string()
+                );
+                Type::Nil
+            }
+            "print" => {
+                print!(
+                    "{}",
+                    params
+                        .get(1)
+                        .expect("The paramater is deficiency")
+                        .get_string()
+                );
+                Type::Nil
+            }
+            "eval" => execute(parse(
+                params
+                    .get(1)
+                    .expect("The paramater is deficiency")
+                    .get_string(),
+            )),
             "exit" => exit(0),
             _ => panic!("美大落ちチョビ髭「チクショーメー」"),
         }
@@ -126,6 +175,9 @@ fn parse(source: String) -> SExpression {
                 .map(|x| parse(x.clone()))
                 .collect::<Vec<SExpression>>(),
         )
+    } else if chars[0] == '"' && chars[chars.len() - 1] == '"' {
+        let inner_string: String = String::from_iter(chars[1..chars.len() - 1].iter());
+        SExpression::Atom(Type::String(inner_string))
     } else {
         let value = source.parse::<f64>();
         SExpression::Atom(if let Ok(i) = value {
@@ -144,10 +196,11 @@ fn tokenize(input: String) -> Vec<String> {
     let mut tokens = Vec::new();
     let mut current_token = String::new();
     let mut in_parentheses = false;
+    let mut in_quote = false;
 
     for c in input.chars() {
         match c {
-            '(' => {
+            '(' if !in_quote => {
                 if in_parentheses {
                     current_token.push(c);
                 } else {
@@ -159,7 +212,7 @@ fn tokenize(input: String) -> Vec<String> {
                     current_token.push(c);
                 }
             }
-            ')' => {
+            ')' if !in_quote => {
                 if in_parentheses {
                     current_token.push(c);
                     in_parentheses = false;
@@ -169,8 +222,19 @@ fn tokenize(input: String) -> Vec<String> {
                     current_token.push(c);
                 }
             }
+            '"' if !in_parentheses => {
+                if in_quote {
+                    current_token.push(c);
+                    in_quote = false;
+                    tokens.push(current_token.clone());
+                    current_token.clear();
+                } else {
+                    in_quote = true;
+                    current_token.push(c);
+                }
+            }
             ' ' | '\n' | '\t' | '\r' | '　' => {
-                if in_parentheses {
+                if in_parentheses || in_quote {
                     current_token.push(c);
                 } else {
                     if !current_token.is_empty() {
