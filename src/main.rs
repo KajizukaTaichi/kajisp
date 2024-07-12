5048ef929ee58d721aeb49c4e0f92b1eaa2c0a92
梶塚太智 <kajizukataichi@outlook.jp>
2024-07-12 22:39:33 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -31,81 +31,101 @@ fn input(prompt: &str) -> String {
 
 #[derive(Clone, Debug)]
 enum SExpression {
-    Atom(Type),
-    List(Vec<SExpression>),
-}
-
-#[derive(Clone, Debug)]
-enum Type {
     Number(f64),
     Bool(bool),
     Symbol(String),
     String(String),
+    List(Vec<SExpression>),
     Nil,
 }
 
-impl Type {
+impl SExpression {
     fn get_number(&self) -> f64 {
         match self {
-            Type::Number(i) => *i,
-            Type::Bool(b) => {
+            SExpression::Number(i) => *i,
+            SExpression::Bool(b) => {
                 if *b {
                     1f64
                 } else {
                     0f64
                 }
             }
-            Type::Symbol(s) => s.parse().unwrap_or(0f64),
-            Type::String(s) => s.parse().unwrap_or(0f64),
-            Type::Nil => 0f64,
+            SExpression::Symbol(s) => s.parse().unwrap_or(0f64),
+            SExpression::String(s) => s.parse().unwrap_or(0f64),
+            SExpression::List(l) => l.len() as f64,
+            SExpression::Nil => 0f64,
         }
     }
 
     fn get_symbol(&self) -> String {
         match self {
-            Type::Number(i) => i.to_string(),
-            Type::Bool(b) => b.to_string(),
-            Type::Symbol(s) => s.to_string(),
-            Type::String(s) => format!("\"{s}\""),
-            Type::Nil => "nil".to_string(),
+            SExpression::Number(i) => i.to_string(),
+            SExpression::Bool(b) => b.to_string(),
+            SExpression::Symbol(s) => s.to_string(),
+            SExpression::String(s) => format!("\"{s}\""),
+            SExpression::List(l) => format!(
+                "({})",
+                l.iter()
+                    .map(|x| x.get_symbol())
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            ),
+            SExpression::Nil => "nil".to_string(),
         }
     }
 
     fn get_string(&self) -> String {
         match self {
-            Type::Number(i) => i.to_string(),
-            Type::Bool(b) => b.to_string(),
-            Type::Symbol(s) => s.to_string(),
-            Type::String(s) => s.to_string(),
-            Type::Nil => String::new(),
+            SExpression::Number(i) => i.to_string(),
+            SExpression::Bool(b) => b.to_string(),
+            SExpression::Symbol(s) => s.to_string(),
+            SExpression::String(s) => s.to_string(),
+            SExpression::List(l) => format!(
+                "({})",
+                l.iter()
+                    .map(|x| x.get_symbol())
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            ),
+            SExpression::Nil => String::new(),
         }
     }
 
     fn get_bool(&self) -> bool {
         match self {
-            Type::Number(i) => *i != 0f64,
-            Type::Bool(b) => *b,
-            Type::Symbol(s) => !s.is_empty(),
-            Type::String(s) => !s.is_empty(),
-            Type::Nil => false,
+            SExpression::Number(i) => *i != 0f64,
+            SExpression::Bool(b) => *b,
+            SExpression::Symbol(s) => !s.is_empty(),
+            SExpression::String(s) => !s.is_empty(),
+            SExpression::List(l) => !l.is_empty(),
+            SExpression::Nil => false,
+        }
+    }
+
+    fn get_list(&self) -> Vec<SExpression> {
+        match self {
+            SExpression::Number(i) => vec![SExpression::Number(*i)],
+            SExpression::Bool(b) => vec![SExpression::Bool(*b)],
+            SExpression::Symbol(s) => vec![SExpression::Symbol(s.clone())],
+            SExpression::String(s) => vec![SExpression::String(s.clone())],
+            SExpression::List(l) => l.clone(),
+            SExpression::Nil => vec![],
         }
     }
 }
 
-fn execute(program: SExpression) -> Type {
-    if let SExpression::Atom(value) = program {
-        return value;
-    } else if let SExpression::List(list) = program {
-        let list: Vec<Type> = list.iter().map(|i| execute(i.clone())).collect();
-        let function: Type = if let Some(i) = list.get(0) {
+fn execute(program: SExpression) -> SExpression {
+    if let SExpression::List(list) = program {
+        let list: Vec<SExpression> = list.iter().map(|i| execute(i.clone())).collect();
+        let function: SExpression = if let Some(i) = list.get(0) {
             i.clone()
         } else {
-            return Type::Nil;
+            return SExpression::Nil;
         };
-        let params: Vec<Type> = list[1..list.len()].to_vec();
+        let params: Vec<SExpression> = list[1..list.len()].to_vec();
 
         match function.get_symbol().as_str() {
-            "+" => Type::Number({
+            "+" => SExpression::Number({
                 let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
 
                 let mut result: f64 = *params.get(0).expect("The paramater is deficiency");
@@ -114,7 +134,7 @@ fn execute(program: SExpression) -> Type {
                 }
                 result
             }),
-            "-" => Type::Number({
+            "-" => SExpression::Number({
                 let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
 
                 let mut result: f64 = *params.get(0).expect("The paramater is deficiency");
@@ -123,7 +143,7 @@ fn execute(program: SExpression) -> Type {
                 }
                 result
             }),
-            "*" => Type::Number({
+            "*" => SExpression::Number({
                 let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
 
                 let mut result: f64 = *params.get(0).expect("The paramater is deficiency");
@@ -132,7 +152,7 @@ fn execute(program: SExpression) -> Type {
                 }
                 result
             }),
-            "/" => Type::Number({
+            "/" => SExpression::Number({
                 let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
 
                 let mut result: f64 = *params.get(0).expect("The paramater is deficiency");
@@ -141,7 +161,7 @@ fn execute(program: SExpression) -> Type {
                 }
                 result
             }),
-            "%" => Type::Number({
+            "%" => SExpression::Number({
                 let params: Vec<f64> = params.iter().map(|i| i.get_number()).collect();
 
                 let mut result: f64 = *params.get(0).expect("The paramater is deficiency");
@@ -150,7 +170,7 @@ fn execute(program: SExpression) -> Type {
                 }
                 result
             }),
-            "=" => Type::Bool({
+            "=" => SExpression::Bool({
                 let params: Vec<String> = params.iter().map(|i| i.get_symbol()).collect();
 
                 match params.first() {
@@ -158,37 +178,37 @@ fn execute(program: SExpression) -> Type {
                     None => true, // ベクタが空の場合はtrueとする
                 }
             }),
-            ">" => Type::Bool({
+            ">" => SExpression::Bool({
                 let params: Vec<String> = params.iter().map(|i| i.get_symbol()).collect();
                 params.windows(2).all(|window| window[0] > window[1])
             }),
-            ">=" => Type::Bool({
+            ">=" => SExpression::Bool({
                 let params: Vec<String> = params.iter().map(|i| i.get_symbol()).collect();
                 params.windows(2).all(|window| window[0] >= window[1])
             }),
-            "<" => Type::Bool({
+            "<" => SExpression::Bool({
                 let params: Vec<String> = params.iter().map(|i| i.get_symbol()).collect();
                 params.windows(2).all(|window| window[0] < window[1])
             }),
-            "<=" => Type::Bool({
+            "<=" => SExpression::Bool({
                 let params: Vec<String> = params.iter().map(|i| i.get_symbol()).collect();
                 params.windows(2).all(|window| window[0] <= window[1])
             }),
-            "&" => Type::Bool({
+            "&" => SExpression::Bool({
                 let params: Vec<bool> = params.iter().map(|i| i.get_bool()).collect();
                 params.iter().all(|&x| x)
             }),
-            "|" => Type::Bool({
+            "|" => SExpression::Bool({
                 let params: Vec<bool> = params.iter().map(|i| i.get_bool()).collect();
                 params.iter().any(|&x| x)
             }),
-            "!" => Type::Bool({
+            "!" => SExpression::Bool({
                 !params
                     .get(0)
                     .expect("The paramater is deficiency")
                     .get_bool()
             }),
-            "concat" => Type::String(params.iter().map(|i| i.get_string()).collect()),
+            "concat" => SExpression::String(params.iter().map(|i| i.get_string()).collect()),
             "println" => {
                 println!(
                     "{}",
@@ -197,7 +217,7 @@ fn execute(program: SExpression) -> Type {
                         .expect("The paramater is deficiency")
                         .get_string()
                 );
-                Type::Nil
+                SExpression::Nil
             }
             "print" => {
                 print!(
@@ -207,31 +227,46 @@ fn execute(program: SExpression) -> Type {
                         .expect("The paramater is deficiency")
                         .get_string()
                 );
-                Type::Nil
+                SExpression::Nil
             }
-            "eval" => execute(parse(
-                params
+            "input" => SExpression::String(input(
+                &params
                     .get(0)
                     .expect("The paramater is deficiency")
                     .get_string(),
             )),
+            "eval" => execute({
+                let params = params
+                    .get(0)
+                    .expect("The paramater is deficiency")
+                    .get_list();
+                SExpression::List(params[1..params.len()].to_vec())
+            }),
             "if" => {
                 let condition = params
                     .get(0)
                     .expect("The paramater is deficiency")
                     .get_bool();
-                if condition {
-                    params.get(1).expect("The paramater is deficiency").clone()
+                let params = if condition {
+                    params.get(1)
                 } else {
-                    params.get(2).expect("The paramater is deficiency").clone()
+                    params.get(2)
+                }
+                .expect("The paramater is deficiency");
+                if let SExpression::List(list) = params {
+                    execute(SExpression::List(list[1..list.len()].to_vec()))
+                } else {
+                    params.clone()
                 }
             }
-
+            "symbol" => SExpression::List(
+                [vec![SExpression::Symbol("symbol".to_string())], params].concat(),
+            ),
             "exit" => exit(0),
             _ => panic!("This function is undefined"),
         }
     } else {
-        return Type::Nil;
+        return program;
     }
 }
 
@@ -247,63 +282,70 @@ fn parse(source: String) -> SExpression {
         )
     } else if chars[0] == '"' && chars[chars.len() - 1] == '"' {
         let inner_string: String = String::from_iter(chars[1..chars.len() - 1].iter());
-        SExpression::Atom(Type::String(inner_string))
+        SExpression::String(inner_string)
     } else {
-        SExpression::Atom(if let Ok(i) = source.parse::<f64>() {
-            Type::Number(i)
+        if let Ok(i) = source.parse::<f64>() {
+            SExpression::Number(i)
         } else if let Ok(b) = source.parse::<bool>() {
-            Type::Bool(b)
+            SExpression::Bool(b)
         } else if source == "nil" {
-            Type::Nil
+            SExpression::Nil
         } else {
-            Type::Symbol(source)
-        })
+            SExpression::Symbol(source)
+        }
     }
 }
 
 fn tokenize(input: String) -> Vec<String> {
     let mut tokens = Vec::new();
     let mut current_token = String::new();
-    let mut in_parentheses = false;
+    let mut in_parentheses: usize = 0;
     let mut in_quote = false;
 
     for c in input.chars() {
         match c {
             '(' if !in_quote => {
-                if in_parentheses {
+                if in_parentheses != 0 {
+                    in_parentheses += 1;
                     current_token.push(c);
                 } else {
                     if !current_token.is_empty() {
                         tokens.push(current_token.clone());
                         current_token.clear();
                     }
-                    in_parentheses = true;
+                    in_parentheses += 1;
                     current_token.push(c);
                 }
             }
             ')' if !in_quote => {
-                if in_parentheses {
+                if in_parentheses != 0 {
                     current_token.push(c);
-                    in_parentheses = false;
-                    tokens.push(current_token.clone());
-                    current_token.clear();
+                    in_parentheses -= 1;
+                    if in_parentheses == 0 {
+                        tokens.push(current_token.clone());
+                        current_token.clear();
+                    }
                 } else {
-                    current_token.push(c);
+                    panic!("Syntax error: invalid end of parentheses")
                 }
             }
-            '"' if !in_parentheses => {
-                if in_quote {
-                    current_token.push(c);
-                    in_quote = false;
-                    tokens.push(current_token.clone());
-                    current_token.clear();
+            '"' => {
+                if in_parentheses == 0 {
+                    if in_quote {
+                        current_token.push(c);
+                        in_quote = false;
+                        tokens.push(current_token.clone());
+                        current_token.clear();
+                    } else {
+                        in_quote = true;
+                        current_token.push(c);
+                    }
                 } else {
-                    in_quote = true;
                     current_token.push(c);
                 }
             }
             ' ' | '\n' | '\t' | '\r' | '　' => {
-                if in_parentheses || in_quote {
+                if in_parentheses != 0 || in_quote {
                     current_token.push(c);
                 } else {
                     if !current_token.is_empty() {
@@ -316,6 +358,13 @@ fn tokenize(input: String) -> Vec<String> {
                 current_token.push(c);
             }
         }
+    }
+
+    if in_parentheses != 0 {
+        panic!("Syntax error: There isn't end of parentheses");
+    }
+    if in_quote {
+        panic!("Syntax error: There isn't end of quote");
     }
 
     if !current_token.is_empty() {
